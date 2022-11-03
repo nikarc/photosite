b0220e0ce4c3d5a518c4550f287fe8012a574eb9
Nick Arcuri <nickarcuri@protonmail.com>
2022-11-03 08:03:28 -0400

    :recycle: Refactor theme hook

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -3,12 +3,26 @@ import { useLayoutEffect, useEffect } from "react";
 const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
-export type Theme = Record<string, string>;
+export enum ThemeMode {
+  Dark = "dark",
+  Light = "light",
+}
 
-export default function useTheme(theme: Theme) {
+export type Theme = {
+  [key in ThemeMode]: Record<string, string>;
+};
+
+export default function useTheme(theme: Theme, darkMode = false) {
   useIsomorphicLayoutEffect(() => {
-    for (const key in theme) {
-      document.documentElement.style.setProperty(`--${key}`, theme[key]);
+    const mode = darkMode ? ThemeMode.Dark : ThemeMode.Light;
+    const _theme = theme[mode];
+
+    if (!_theme) return;
+
+    for (const key in _theme) {
+      if (!_theme?.[key]) continue;
+
+      document.documentElement.style.setProperty(`--${key}`, _theme[key]);
     }
   }, [theme]);
 }
