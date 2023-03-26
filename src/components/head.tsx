4c57c1724604eb5fb9fc7298cb596946a446efa5
Nicholas Arcuri <nickarcuri@protonmail.com>
2023-03-25 21:52:50 -0400

    Clickable pics

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,5 +1,6 @@
 import NextHead from "next/head";
 import { FragmentType, graphql, useFragment } from "src/gql";
+import styles from "../styles/global.module.scss";
 
 export const SeoFragment = graphql(/* GraphQL */ `
   fragment HeadItems on Seo {
@@ -20,21 +21,24 @@ const Head = (props: { seo: FragmentType<typeof SeoFragment> }) => {
   const seo = useFragment(SeoFragment, props.seo);
 
   return (
-    <NextHead>
-      <title>{seo.metaTitle}</title>
-      <meta name="description" content={seo.metaDescription ?? ""} />
-      <meta
-        property="og:url"
-        content={process.env.NEXT_PUBLIC_SITE_DOMAIN ?? ""}
-      />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:title"
-        content={`Nick Arcuri Photo | ${seo.metaTitle}`}
-      />
-      <meta property="og:description" content={seo.metaDescription ?? ""} />
-      <meta property="og:image" content={seo.ogImage?.url} />
-    </NextHead>
+    <>
+      <NextHead>
+        <title>{seo.metaTitle}</title>
+        <meta name="description" content={seo.metaDescription ?? ""} />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_SITE_DOMAIN ?? ""}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content={`Nick Arcuri Photo | ${seo.metaTitle}`}
+        />
+        <meta property="og:description" content={seo.metaDescription ?? ""} />
+        <meta property="og:image" content={seo.ogImage?.url} />
+      </NextHead>
+      <h1 className={styles.page_title}>{seo.metaTitle}</h1>
+    </>
   );
 };
 
